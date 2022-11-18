91de77a5076ae000c34247d5b63bf94c1450cabd
Juane <juanehueda885@gmail.com>
2022-11-18 20:42:31 -0300

    funcionalidades nuevas

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const carrito = document.querySelector('.cart');
 const productosContainer = document.querySelector('.products-container'); 
+const cartContainer = document.querySelector('.cart-container');
+const total = document.getElementById('cart-total');
 const btnAll = document.getElementById('btnAll');
 const btnMalbec = document.getElementById('btnMalbec');
 const btnCabernet = document.getElementById('btnCabernet');
@@ -11,9 +13,12 @@ const btnBlanc = document.getElementById('btnBlanc');
 
 const categoriaBtn = [btnAll,btnBlanc,btnCabernet,btnMalbec,btnRose]
 
-const arrayCarrito = [];
+let arrayCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-
+// funcion para guardar en localStorage 
+const guardarLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(arrayCarrito));
+}
 
 
 
@@ -27,22 +32,64 @@ const mostrarCarrito = () => {
 const agregarAlCarrito = (id) => {
     let productoAgregado = productsData.find((producto) => {
         if(producto.id === id) return true
-    }) 
-    arrayCarrito.push(productoAgregado);
-} 
+    }); 
+    let productoExistente = arrayCarrito.find(producto => producto.id === id) 
+    if (productoExistente) {
+        productoExistente.quantity ++;
+    } else { arrayCarrito.push({
+        name: productoAgregado.name,
+        id: productoAgregado.id,
+        bid: productoAgregado.bid,
+        quantity: 1
+    });}
+    renderProductosCarrito();
+    guardarLocalStorage();
+}  
+
+// funcion para el boton de restar producto en el carrito
+const restarProducto = (id) => {
+    let productoExistente = arrayCarrito.find(producto => producto.id === id)
+    if (productoExistente.quantity > 1) {
+        productoExistente.quantity--;
+    } else {
+        arrayCarrito.splice(arrayCarrito.indexOf(productoExistente),1);
+    }
+    renderProductosCarrito();
+}
 
 // funcion para renderizar los productos del carrito
 const renderProductosCarrito = () => {
-    
-}
+    let cardsCarrito = arrayCarrito.map(f => {
+        return `<div class="cart-card">
+            <span class="product-name">${f.name}</span>
+            <span>$${f.bid}</span>
+            <div class="card-quantity">
+                <i class="fas fa-chevron-down" onclick="restarProducto(${f.id})"></i>
+                <span>${f.quantity}</span>
+                <i class="fas fa-chevron-up" onclick="agregarAlCarrito(${f.id})"></i>
+            </div>
+        </div>`
+    }); 
+    cartContainer.innerHTML = cardsCarrito.join("");
+} 
+
+// funcion para vaciar el carrin 
+const vaciarCarrito = () => {
+    arrayCarrito = []; renderProductosCarrito();
+};
+
+
 
 // Funcion para renderizar los productos y filtrarlos
+let cantidadRender = 8;
+let activeCategory = 'all';
 const renderProductos = (category) => {
+    activeCategory = category;
     let productosFiltrados = productsData.filter(f => {
         if( category === 'all') return true;
         return f.category === category;
     })  
-    let productosDivididos = dividirProductos(productosFiltrados, 8);
+    let productosDivididos = dividirProductos(productosFiltrados, cantidadRender);
     productosContainer.innerHTML = productosDivididos.map(i => {
         return renderProducto(i);
     }) .join("");
@@ -53,6 +100,12 @@ const dividirProductos = (productos, cantidad) => {
     let productosDivididos = productos.splice(0, cantidad)
     return productosDivididos;
 }; 
+
+// funcion para mostar mas productos
+const mostrarMas = () => {
+    cantidadRender += 8;
+    renderProductos(activeCategory);
+}
 
 // funcion para renderizar un producto 
 const renderProducto = (producto) => { 
@@ -92,6 +145,7 @@ const selectCategory = (event) => {
     let eventDataSet = event.target.dataset;
     pintarCategoria(eventDataSet.category);
     renderProductos(eventDataSet.category);
+    cantidadRender = 8;
 } 
 
 
@@ -101,7 +155,8 @@ const selectCategory = (event) => {
 
 
 const init = () => {
-    renderProductos('all')
+    renderProductos('all');
+    renderProductosCarrito();
 }
 
 init();
